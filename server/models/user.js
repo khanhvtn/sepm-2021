e48353c71770c86c5955325146029dbbb61ce612
khanhvtn <khanhvotrannhut93@gmail.com>
2021-03-17 15:04:54 +0700

    add timesamps for user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
-const userSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    points: { type: String, default: '0' },
-    accountBalance: { type: String, default: '0' },
-});
+const userSchema = mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        points: { type: String, default: '0' },
+        accountBalance: { type: String, default: '0' },
+    },
+    { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 export default User;
