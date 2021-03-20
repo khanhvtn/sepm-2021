125aa7580a1a827c1c059ae653d7f97e983578e2
khanhvtn <khanhvotrannhut93@gmail.com>
2021-03-20 22:12:08 +0700

    add component Games

---
+++ client/src/components/Games/Games.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+import useStyles from './styles';
+const Games = () => {
+    const classes = useStyles();
+    return <Typography>List Game</Typography>;
+};
+
+export default Games;
