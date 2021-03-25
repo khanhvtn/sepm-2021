733327782017a681141ac72c0edc25fa3d0eb9df
lamdoan7747 <67705960+lamdoan7747@users.noreply.github.com>
2021-03-25 17:15:42 +0700

    Update admin signin backend

--- server/controllers/admins.js
+++ server/controllers/admins.js
@@ -2,6 +2,7 @@ import User from '../models/user.js';
 import Brand from '../models/brand.js';
 import Voucher from '../models/voucher.js';
 import mongoose from 'mongoose';
+import Admin from '../models/admin.js';
 
 
 export const getUsers = async (req, res) => {
@@ -31,7 +32,7 @@ export const deleteUser = async (req, res) => {
 export const setVoucher = async (req, res) => {
     const { id: _id } = req.params;
     const { type } = req.body
-    
+
     const action = type === 'ACCEPT' ? true : false
 
     if (!mongoose.Types.ObjectId.isValid(_id)) {
@@ -48,3 +49,31 @@ export const setVoucher = async (req, res) => {
         res.status(400).json({ message: error.message })
     }
 };
+
+export const signin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const existingAdmin = await Admin.findOne({ email });
+        //response error if user doesn't exist.
+        if (!existingUser) {
+            return res.status(404).json({ message: "Admmin doesn't exist" });
+        }
+
+        const isPasswordCorrect = existingAdmin.password === password ? true : false
+        //response error if password is incorrect
+        if (!isPasswordCorrect) {
+            return res.status(404).json({ message: 'Invalid credentials.' });
+        }
+        
+        const token = jwt.sign(
+            { email: existingAdmin.email, id: existingAdmin._id },
+            'vouchy123',
+            { expiresIn: '1h' }
+        );
+
+        //response success
+        res.status(200).json({ result: existingAdmin, token });
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong.' });
+    }
+}
--- server/routes/admins.js
+++ server/routes/admins.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getUsers, setVoucher, deleteUser } from '../controllers/admins.js';
+import { getUsers, setVoucher, deleteUser, signin } from '../controllers/admins.js';
 
 const router = express.Router();
 
 router.patch('/setVoucher/:id', setVoucher);
 router.get('/users', getUsers);
 router.delete('/users/:id', deleteUser);
+router.post('/signin', signin)
 
 export default router;
