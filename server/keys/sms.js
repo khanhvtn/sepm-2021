27fc1c4202a073a54cfa0bbd66be7379975f03bb
s3678460 <40556454+s3678460@users.noreply.github.com>
2021-04-14 11:47:21 +0700

    minh commit

--- server/keys/sms.js
+++ server/keys/sms.js
@@ -1,4 +1,4 @@
 export const smsKey = {
     ACCOUNT_SID: "AC763cfe4f372695320e25d26cf50f2054",
-    AUTH_TOKEN: "7e4594e7e3c3a6a194d79ceeff38680e"
+    AUTH_TOKEN: "e19f35a83e0e10a9e0cdbaee7c31adc7"
 }
