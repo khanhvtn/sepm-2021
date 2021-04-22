945759e39eaa71e10a3d9d89f0910c70b917c55d
lamdoan <doansonlam7747@gmail.com>
2021-04-22 15:36:17 +0700

    Update git ignore and env

--- server/keys/sms.js
+++ server/keys/sms.js
@@ -1,4 +1,8 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const smsKey = {
-    ACCOUNT_SID: "AC763cfe4f372695320e25d26cf50f2054",
-    AUTH_TOKEN: "e19f35a83e0e10a9e0cdbaee7c31adc7"
+    ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+    AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN
 }
