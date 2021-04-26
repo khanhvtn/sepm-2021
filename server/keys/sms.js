aa7dea996cec344b49cb89241d4f1b698d44484e
s3678460 <40556454+s3678460@users.noreply.github.com>
2021-04-26 14:42:26 +0700

    Update sms.js

--- server/keys/sms.js
+++ server/keys/sms.js
@@ -7,7 +7,3 @@ export const smsKey = {
     AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN
 }
 
-// export const smsKey = {
-//     ACCOUNT_SID: "AC763cfe4f372695320e25d26cf50f2054",
-//     AUTH_TOKEN: "e199a5b0c5b3794d2608ad88010e33ab"
-// }
