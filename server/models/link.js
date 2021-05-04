6ff5b10c6288d4176f4ec80dec90d236b8ddd11b
s3678460 <40556454+s3678460@users.noreply.github.com>
2021-05-04 09:20:04 +0700

    minh

--- server/models/link.js
+++ server/models/link.js
@@ -10,14 +10,14 @@ const linkSchema = mongoose.Schema(
             type: mongoose.Schema.Types.ObjectId,
             required: true
         },
-        createdAt: { type: Date, default: Date.now, index: { unique: true} }
+        createdAt: { type: Date, default: Date.now, index: true }
        
 
     },
     // { timestamps: true }
 );
 
-linkSchema.index({ createdAt: 1 }, {expires: "3m"} )
+linkSchema.index({ createdAt: 1 }, {expireAfterSeconds: 3600 } )
 
 const Link = mongoose.model('Link', linkSchema);
 export default Link;
